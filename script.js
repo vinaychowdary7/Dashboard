37052d02076d740962c0a764fb862568ec702098
Vinay Chowdary <munnangivinay357@gmail.com>
2023-11-30 12:56:57 +0530

    first commit

--- script.js
+++ script.js
@@ -100,4 +100,3 @@ document.querySelector('#closeAccountForm').addEventListener('input', function (
   var formIsValid = document.querySelector('#closeAccountForm').checkValidity();
   document.querySelector('#form-but').disabled = !formIsValid;
 });
-// code is completed.
